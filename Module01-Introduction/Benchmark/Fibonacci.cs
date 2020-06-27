4979ea70fb7ff68790aab9e53ad0f791cbd1e261
evilpilaf <mauriciod73@gmail.com>
2020-06-27 17:23:05 +0200

    Add implementation for Fibonacci iterative and recursive with memorization

--- Module01-Introduction/Benchmark/Fibonacci.cs
+++ Module01-Introduction/Benchmark/Fibonacci.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
+
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using BenchmarkDotNet.Jobs;
 
 namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
 {
     [DisassemblyDiagnoser(exportCombinedDisassemblyReport: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
     public class FibonacciCalc
     {
         // HOMEWORK:
@@ -11,8 +16,16 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
         // 2. Add MemoryDiagnoser to the benchmark
         // 3. Run with release configuration and compare results
         // 4. Open disassembler report and compare machine code
-        // 
+        //
         // You can use the discussion panel to compare your results with other students
+
+        private readonly ulong?[] _fibonacciValues = new ulong?[1000];
+
+        public FibonacciCalc()
+        {
+            _fibonacciValues[0] = 0;
+            _fibonacciValues[1] = 1;
+        }
 
         [Benchmark(Baseline = true)]
         [ArgumentsSource(nameof(Data))]
@@ -26,18 +39,40 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
         [ArgumentsSource(nameof(Data))]
         public ulong RecursiveWithMemoization(ulong n)
         {
-            return 0;
+            var val = _fibonacciValues[n];
+            if (!val.HasValue)
+            {
+                val = RecursiveWithMemoization(n - 1) + RecursiveWithMemoization(n - 2);
+                _fibonacciValues[n] = val;
+            }
+            return val.Value;
         }
-        
+
         [Benchmark]
         [ArgumentsSource(nameof(Data))]
         public ulong Iterative(ulong n)
         {
-            return 0;
+            if (n == 0) return 0;
+            if (n == 1 || n == 2) return 1;
+
+            ulong a = 0;
+            ulong b = 1;
+            ulong c = 0;
+
+            for (ulong i = 2; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
         }
 
         public IEnumerable<ulong> Data()
         {
+            yield return 1;
+            yield return 3;
             yield return 15;
             yield return 35;
         }
