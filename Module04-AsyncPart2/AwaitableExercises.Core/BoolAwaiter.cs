3745b72024e613e9b04f67146ef22c930c5888a2
evilpilaf <mauriciod73@gmail.com>
2020-07-21 20:30:14 +0200

    Module 3 resolved

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        //public static TaskAwaiter<bool> GetAwaiter(this bool value)
+        //    => Task.FromResult(value).GetAwaiter();
+        public static BoolAwaiter GetAwaiter(this bool value)
+            => new BoolAwaiter(value);
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool _value;
+
+        public bool GetResult() => _value;
+        public bool IsCompleted => true;
+
+        public BoolAwaiter(bool value)
+        {
+            _value = value;
+        }
+
+        public BoolAwaiter GetAwaiter() => this;
+
         public void OnCompleted(Action continuation)
         {
-            throw new NotImplementedException();
+            continuation();
         }
     }
 }
--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -8,7 +8,36 @@ namespace TaskCompletionSourceExercises.Core
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            var process = new Process
+            {
+                EnableRaisingEvents = true,
+                StartInfo = new ProcessStartInfo(path, args)
+                {
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true
+                }
+            };
+
+            process.Exited += async (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+                Debug.Assert(senderProcess != null, nameof(senderProcess) + " != null");
+                if (senderProcess.ExitCode != 0)
+                {
+                    var error = await senderProcess.StandardError.ReadToEndAsync();
+                    tcs.SetException(new Exception(error));
+                }
+                else
+                {
+                    var result = await senderProcess.StandardOutput.ReadToEndAsync();
+                    tcs.SetResult(result);
+                }
+            };
+            process.Start();    
+
+            return tcs.Task;
         }
     }
 }
